52a7f1b3767d4b61db6c1a9c2feda03c51b82a35
Jeffrey Lee <jlee42@gmail.com>
2015-05-13 13:49:33 -0500

    Add NSNumberFormatter support to Floats

--- Source/Formatters/FORMFloatFormatter.m
+++ Source/Formatters/FORMFloatFormatter.m
@@ -2,15 +2,27 @@
 
 @implementation FORMFloatFormatter
 
-- (NSString *)formatString:(NSString *)string reverse:(BOOL)reverse {
+- (NSString *)formatString:(NSString *)string reverse:(BOOL)reverse formatter:(NSNumberFormatter *)formatter {
     string = [super formatString:string reverse:reverse];
     if (!string) return nil;
+    string = [string stringByReplacingOccurrencesOfString:@"." withString:formatter.decimalSeparator];
 
+    NSNumber *number = [formatter numberFromString:string];
+  
     if (reverse) {
-        return [string stringByReplacingOccurrencesOfString:@"," withString:@"."];
+        [formatter setNumberStyle:NSNumberFormatterNoStyle];
     } else {
-        return [string stringByReplacingOccurrencesOfString:@"." withString:@","];
+        [formatter setNumberStyle:NSNumberFormatterDecimalStyle];
     }
+    string = [formatter stringFromNumber:number];
+    return string;
+}
+
+- (NSString *)formatString:(NSString *)string reverse:(BOOL)reverse {
+  NSNumberFormatter *formatter = [NSNumberFormatter new];
+  formatter.positiveFormat = @"###.##";
+  formatter.decimalSeparator = @",";
+  return [self formatString:string reverse:reverse formatter:formatter];
 }
 
 @end
