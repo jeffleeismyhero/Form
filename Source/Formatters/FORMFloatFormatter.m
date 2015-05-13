fff9b7da2ea4961ae9d04df2a950db61aa325404
Jeffrey Lee <jlee42@gmail.com>
2015-05-13 10:49:06 -0500

    Allow dot entry in float string.
    The dot is currently replaced with a comma upon entry. This maintains existing functionality and adds support for automatically converting a dot to a comma.
    
    Add NSNumberFormatter support to Floats
    
    Add tests for Float formatting with a formatter
    
    Add field attributes for number formatting
    
    - positiveFormat: Format of positive numbers
    - negativeFormat: Format of negative numbers (defaults to positiveFormat with a - in front)
    - groupingSeparator: Separator for whole numbers (e.g., 1,000.00 or 1 000,00)
    - decimalSeparator: Separator for decimal place (e.g., 1.23 or 1,23)
    
    Add JSON file with formatted float samples

--- Source/Formatters/FORMFloatFormatter.m
+++ Source/Formatters/FORMFloatFormatter.m
@@ -5,21 +5,21 @@
 - (NSString *)formatString:(NSString *)string reverse:(BOOL)reverse formatter:(NSNumberFormatter *)formatter {
     string = [super formatString:string reverse:reverse formatter:formatter];
     if (!string) return nil;
-
+    
     if (reverse) {
-	return [string stringByReplacingOccurrencesOfString:formatter.decimalSeparator withString:@"."];
+        return [string stringByReplacingOccurrencesOfString:formatter.decimalSeparator withString:@"."];
     } else {
-	string = [string stringByReplacingOccurrencesOfString:@"." withString:formatter.decimalSeparator];
-	return [string stringByReplacingOccurrencesOfString:@"" withString:formatter.groupingSeparator];
+        string = [string stringByReplacingOccurrencesOfString:@"." withString:formatter.decimalSeparator];
+        return [string stringByReplacingOccurrencesOfString:@"" withString:formatter.groupingSeparator];
     }
 }
 
 - (NSString *)formatString:(NSString *)string reverse:(BOOL)reverse {
-  NSNumberFormatter *formatter = [NSNumberFormatter new];
-  formatter.groupingSeparator = @"";
-  formatter.decimalSeparator = @",";
+    NSNumberFormatter *formatter = [NSNumberFormatter new];
+    formatter.groupingSeparator = @"";
+    formatter.decimalSeparator = @",";
 
-  return [self formatString:string reverse:reverse formatter:formatter];
+    return [self formatString:string reverse:reverse formatter:formatter];
 }
 
 @end
