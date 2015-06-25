8e2184482d7a25b7c75fe258ff8952018e2154f7
Jeffrey Lee <jlee42@gmail.com>
2015-06-25 12:42:35 -0500

    Add option to pass a formatter to FORMFormatter

--- Source/Formatters/FORMFormatter.h
+++ Source/Formatters/FORMFormatter.h
@@ -3,5 +3,6 @@
 @interface FORMFormatter : NSObject
 
 - (NSString *)formatString:(NSString *)string reverse:(BOOL)reverse;
+- (NSString *)formatString:(NSString *)string reverse:(BOOL)reverse formatter:(NSNumberFormatter *)formatter;
 
 @end
